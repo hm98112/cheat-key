0005a48985bf250f6883d2618b2d739b1ecc5efc
ParkSSing-dev <dbrldhr1918@naver.com>
2025-07-16 14:06:53 +0900

    Remove input and button components from App and update README with Git usage instructions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,5 @@
 import { useState } from 'react'
 
 export default function App() {
-  const [text, setText] = useState('Hello~')
-
-  return (
-    <>
-      <input
-        type="text"
-        value={text}
-        onChange={function (event) {
-          setText(event.target.value)
-        }}
-      />
-      <button
-        onClick={function () {
-          console.log(text)
-        }}>
-        LogIn
-      </button>
-      <h1>{text}</h1>
-    </>
-  )
+  return <></>
 }
