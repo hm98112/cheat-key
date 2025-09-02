22029e27dcadf63073b8c261b4f9cdc6b57349c5
ParkSSing-dev <psu8736@gmail.com>
2025-09-02 09:13:34 +0900

    Front-End 개발중

--- client/src/routes/pages/SignIn.jsx
+++ client/src/routes/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import AuthPage from 'C:/Users/user/OneDrive/바탕 화면/Final Azure 자료/CheatKey Project/client/src/components/login/AuthPage.jsx';
+import AuthPage from '@/components/login/AuthPage.jsx'
 
 export default function SignIn() {
   return (
